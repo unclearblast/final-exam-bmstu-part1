af13c5b6d3c9b357d0cadd618126977e0de5f4e1
unclearblast <derenikhm@gmail.com>
2026-03-15 21:33:20 +0300

    Create Reader.java

---
+++ src/main/java/model/Reader.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Reader {
+
+    private int id;
+    private String name;
+    private String email;
+
+    public Reader(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Reader(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+}
